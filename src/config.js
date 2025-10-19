a76b2e01451b5bdf71dd96e9af4c0f3ee55bc2f2
Justinr922 <justinr922@gmail.com>
2025-10-19 17:26:18 -0400

    Update Projects

--- src/config.js
+++ src/config.js
@@ -16,26 +16,6 @@ export const CONFIG = {
   ],
   projects: [
     {
-      title: "Strava Activity Analytics",
-      blurb:
-        "React + Node app for visualizing pace, HR, elevation, and other metrics.",
-      tags: ["React", "Node", "Tailwind", "Strava API"],
-      url: "https://strava-dashboard-v1.onrender.com/",
-      repo: "justinr922/strava-dashboard",
-      inProgress: false,
-      hideRepo: false,
-    },
-    {
-      title: "Power BI Service Manager (Concept)",
-      blurb:
-        "Web tool for consultants/ISVs to manage tenants, workspaces, semantic models, and reports.",
-      tags: ["Power BI", "Azure AD", "REST APIs", "SaaS"],
-      url: "null",
-      repo: "null",
-      inProgress: true,
-      hideRepo: true,
-    },
-    {
       title: "CMS Analytics (In Partnership with FSI and Consertus)",
       blurb:
         "Improve your ROI with data-backed insights, benchmark assets, and monitor team performance.",
@@ -46,14 +26,44 @@ export const CONFIG = {
       hideRepo: true,
     },
     {
-      title: "Etch a Sketch",
+      title: "Wasted Space - Improving Garage Utilization and Revenue",
       blurb:
-        "An old learning project to learn the basics of DOM interaction and Github Pages",
-      tags: ["Web Dev", "DOM Manipulation", "Javascript", "Learning"],
-      url: "https://justinr922.github.io/etch-a-sketch/",
-      repo: "https://github.com/justinr922/etch-a-sketch",
+        "A custom project for a client to understand and optimize usage of their garage portfolio, leading to $100k+ in additional yearly revenue.",
+      tags: ["Integrations", "Business Intelligence", "Data Engineering", "Process Optimization"],
+      url: null,
+      repo: null,
+      inProgress: false,
+      hideRepo: true,
+    },
+    {
+      title: "Strava Activity Analytics",
+      blurb:
+        "React + Node app for visualizing pace, HR, elevation, and other metrics.",
+      tags: ["React", "Node", "Tailwind", "Strava API"],
+      url: "https://strava-dashboard-v1.onrender.com/",
+      repo: "justinr922/strava-dashboard",
       inProgress: false,
       hideRepo: false,
+    },
+    {
+      title: "It's (not) Free Real Estate - Understanding Vacancy and Unit Turnover",
+      blurb:
+        "A custom project for a client to understand and optimize usage of their rental portfolio, allowing for better planning and decision making in an ever changing post pandemic real estate market.",
+      tags: ["Integrations", "Business Intelligence", "Data Engineering", "Process Optimization"],
+      url: null,
+      repo: null,
+      inProgress: false,
+      hideRepo: true,
+    },
+    {
+      title: "Power BI Service Manager (Concept)",
+      blurb:
+        "Web tool for consultants/ISVs to manage tenants, workspaces, semantic models, and reports.",
+      tags: ["Power BI", "Azure AD", "REST APIs", "SaaS"],
+      url: "null",
+      repo: "null",
+      inProgress: true,
+      hideRepo: true,
     },
     {
       title: "Intelligent Cashflow Forecasting",
@@ -74,6 +84,16 @@ export const CONFIG = {
       repo: null,
       inProgress: false,
       hideRepo: true,
+    },
+    {
+      title: "Etch a Sketch",
+      blurb:
+        "An old learning project to learn the basics of DOM interaction and Github Pages",
+      tags: ["Web Dev", "DOM Manipulation", "Javascript", "Learning"],
+      url: "https://justinr922.github.io/etch-a-sketch/",
+      repo: "https://github.com/justinr922/etch-a-sketch",
+      inProgress: false,
+      hideRepo: false,
     },
   ],
   services: [
